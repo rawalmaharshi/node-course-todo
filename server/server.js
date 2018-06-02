55d1b752ce92d530cd2ec0ec85359d8d0db54ed1
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-02 10:38:20 +0530

    code for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send(e));
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started at port ${port}`);
 });
 
 module.exports = {app};
