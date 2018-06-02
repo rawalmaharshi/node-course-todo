66f56511340dfdc91358b219535c14bd2694b253
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-02 22:37:51 +0530

    Setup test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PROD_MONGODB);
 
 module.exports = {mongoose};
