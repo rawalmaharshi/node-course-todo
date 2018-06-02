f85f4193ca9cacb20297edf908fd5d11fb3de9f0
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-02 10:18:08 +0530

    Tests added for /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user')
+var {User} = require('./models/user');
+
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -28,6 +30,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid ID');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send('Todo not found for that id.');
+        }
+
+        // res.status(200).send(todo); don't send it like this, use {todo} instead
+        res.status(200).send({todo});
+    }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(3000, () => {
