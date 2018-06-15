a67cfe1d9de831634c98702050c1e4f92c633738
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-15 18:32:58 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,19 @@
 var env = process.env.NODE_ENV || 'development';
 // console.log('env *******', env);
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    // console.log(config);
+
+    var envConfig = config[env]; //If you want a variable to access an object property, you have to use brakcet notation.
+    Object.keys(envConfig).forEach((key) =>{
+        process.env[key] = envConfig[key];
+    });
 }
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoAppTest';
+// }
