fec5c96e575c0c0be88f3584c4facc848ae678eb
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-15 16:11:41 +0530

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -114,6 +114,18 @@ UserSchema.pre('save', function (next) {
     }
 });
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
